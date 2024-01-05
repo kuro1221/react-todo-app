a847263a934613c386220ac0f8cf44a49f56a584
kuro <toshras22@gmail.com>
2024-01-06 06:43:07 +0900

    implement feature to add new Todo items

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, ChangeEvent } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState<string[]>(['test'])
+  const [todos, setTodos] = useState<string[]>([''])
+  const [text, setText] = useState<string>('')
 
-  const addTodo = () => {}
+  const onClickAdd = () => {
+    const newTodos = [...todos]
+    newTodos.push(text)
+    setTodos(newTodos)
+    setText('')
+  }
+
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }
 
   return (
     <div className="App">
       {todos.map((todo, index) => (
         <p>{todo}</p>
       ))}
-      <button onClick={addTodo}>追加</button>
+      <input type="text" value={text} onChange={onChangeText} />
+      <button onClick={onClickAdd}>追加</button>
     </div>
   )
 }
