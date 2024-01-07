c93d395feba573df23e067beb46ff6cb222e3cc8
kuro <toshras22@gmail.com>
2024-01-07 09:01:30 +0900

    ボタンのclassNameを共通化

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
         />
         <button
           onClick={onClickAdd}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-blue-500 hover:bg-blue-700 btn"
         >
           追加
         </button>
